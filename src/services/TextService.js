4870ba392566bdd33de86365d7ffac86bb7a9bc7
Jennifer Moore <jenniferplusplus@gmail.com>
2019-05-15 19:23:22 -0500

    added random case method

--- src/services/TextService.js
+++ src/services/TextService.js
@@ -2,8 +2,21 @@ function capitalize(text) {
   return String(text).toUpperCase();
 }
 
+function randomCase(text) {
+  let s = String(text).toLocaleLowerCase().split('');
+
+  let result = s.map((val) => {
+    return Math.random() > 0.50
+      ? val.toUpperCase()
+      : val;
+  });
+
+  return result.join('');
+}
+
 const service = {
-  capitalize
+  capitalize,
+  randomCase
 };
 
 
--- src/services/TextService.spec.js
+++ src/services/TextService.spec.js
@@ -3,11 +3,21 @@ import TextService from './TextService'
 
 test('exists', () => {
   expect(TextService).not.toBe(undefined)
-})
+});
 
 test('should capitalize', () => {
   const text = 'lowercase';
   let actual = TextService.capitalize(text);
 
   expect(actual).toBe('LOWERCASE');
+});
+
+test('should randomize the case', () => {
+  const text = 'aaaaaaaaaa';
+  let actual = TextService.randomCase(text);
+
+  expect(actual).toContain('a');
+  expect(actual).toContain('A');
+  expect(actual).not.toBe(text);
+
 })
