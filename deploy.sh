5c7005247dcc99d3752118e8fecc8e8ffa04068f
Jennifer Moore <jenniferplusplus@gmail.com>
2019-05-16 22:05:44 -0500

    use ssh for deploy

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/jenniferplusplus/clapbackplusplus.git master:gh-pages
+git push -f git@github.com:jenniferplusplus/clapbackplusplus.git master:gh-pages
 
 cd -
